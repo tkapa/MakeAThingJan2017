9a69dc1f8705ee0b5297a87276f190ed12849b3b
Unknown <1005787@student.sae.edu.au>
2018-01-22 22:42:15 +1000

    added temp/first pass models for player + slime

---
+++ Assets/character/simpleCharMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleCharMovement : MonoBehaviour
+{
+
+    public float minAngle,maxAngle,normalAngle;
+    public float animSpeed;
+    private float currAngle;
+
+    void Start()
+    {
+        currAngle = normalAngle;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+	{
+	    
+        //if (movement){}
+
+        //Testing 
+
+	    if (currAngle <= minAngle)
+	    {
+
+	    }
+	    else if(currAngle >= maxAngle )
+	    {
+	        
+	    }
+
+	    transform.eulerAngles = new Vector3(currAngle, transform.position.y, transform.position.z);
+    }
+}
